76e4a287b80ac4ca87677e4d6698459e189ad3bc
Patrik Chupáč <chupac10@stud.uniza.sk>
2024-10-15 16:25:41 +0200

    Project setup, Home page

---
+++ movies/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Movie
+
+def home(request):
+    return render(request, 'movies/home.html')
+
+def movies_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies/movies_list.html', {'movies': movies})
+
+def movie_detail(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    return render(request, 'movies/movie_detail.html', {'movie': movie})
